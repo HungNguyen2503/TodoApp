5f8b95d78bd83988388d335ccad9a0b67621c3a6
NguyenPhiHung <hungmun42@gmail.com>
2025-08-29 14:06:18 +0700

    fix dynamic routes conflict on vercel

--- api/todos/[id]/delete.js
+++ api/todos/[id]/delete.js
@@ -1,4 +1,4 @@
-import { User } from '../../_db.js';
+import { User, dbConnect } from '../../_db.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'DELETE') {
@@ -13,6 +13,9 @@ export default async function handler(req, res) {
   }
 
   try {
+    // Connect to database
+    await dbConnect();
+    
     const user = await User.findOne({ token });
     if (!user) {
       return res.status(401).json({ error: 'invalid token' });
@@ -20,14 +23,14 @@ export default async function handler(req, res) {
 
     const { id } = req.query;
     
-    // Find and remove todo by _id
-    const todo = user.todos.id(id);
-    if (!todo) {
+    // Find and remove todo using JavaScript filter instead of Mongoose deleteOne()
+    const todoExists = user.todos.some(todo => todo._id.toString() === id);
+    if (!todoExists) {
       return res.status(404).json({ error: 'todo not found' });
     }
 
-    // Remove the todo from embedded array
-    todo.deleteOne();
+    // Filter out the todo to delete it
+    user.todos = user.todos.filter(todo => todo._id.toString() !== id);
     await user.save();
     
     return res.status(204).end();
@@ -36,3 +39,41 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'server error' });
   }
 }
+// import { User } from '../../_db.js';
+
+// export default async function handler(req, res) {
+//   if (req.method !== 'DELETE') {
+//     return res.status(405).json({ error: 'method not allowed' });
+//   }
+
+//   const authHeader = req.headers.authorization || '';
+//   const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
+  
+//   if (!token) {
+//     return res.status(401).json({ error: 'missing token' });
+//   }
+
+//   try {
+//     const user = await User.findOne({ token });
+//     if (!user) {
+//       return res.status(401).json({ error: 'invalid token' });
+//     }
+
+//     const { id } = req.query;
+    
+//     // Find and remove todo by _id
+//     const todo = user.todos.id(id);
+//     if (!todo) {
+//       return res.status(404).json({ error: 'todo not found' });
+//     }
+
+//     // Remove the todo from embedded array
+//     todo.deleteOne();
+//     await user.save();
+    
+//     return res.status(204).end();
+//   } catch (error) {
+//     console.error('Delete todo error:', error);
+//     return res.status(500).json({ error: 'server error' });
+//   }
+// }
--- api/todos/[id]/toggle.js
+++ api/todos/[id]/toggle.js
@@ -1,4 +1,4 @@
-import { User } from '../../_db.js';
+import { User, dbConnect } from '../../_db.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -13,6 +13,9 @@ export default async function handler(req, res) {
   }
 
   try {
+    // Connect to database
+    await dbConnect();
+    
     const user = await User.findOne({ token });
     if (!user) {
       return res.status(401).json({ error: 'invalid token' });
@@ -20,21 +23,61 @@ export default async function handler(req, res) {
 
     const { id } = req.query;
     
-    // Find todo by _id in embedded array
-    const todo = user.todos._id(id);
-    if (!todo) {
+    // Find todo using JavaScript instead of Mongoose .id() method
+    const todoIndex = user.todos.findIndex(todo => todo._id.toString() === id);
+    if (todoIndex === -1) {
       return res.status(404).json({ error: 'not found' });
     }
 
     // Toggle done status
-    todo.done = !todo.done;
-    todo.updatedAt = new Date();
+    user.todos[todoIndex].done = !user.todos[todoIndex].done;
+    user.todos[todoIndex].updatedAt = new Date();
     
     await user.save();
     
-    return res.json(todo);
+    return res.json(user.todos[todoIndex]);
   } catch (error) {
     console.error('Toggle todo error:', error);
     return res.status(500).json({ error: 'server error' });
   }
 }
+// import { User } from '../../_db.js';
+
+// export default async function handler(req, res) {
+//   if (req.method !== 'PATCH') {
+//     return res.status(405).json({ error: 'method not allowed' });
+//   }
+
+//   const authHeader = req.headers.authorization || '';
+//   const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
+  
+//   if (!token) {
+//     return res.status(401).json({ error: 'missing token' });
+//   }
+
+//   try {
+//     const user = await User.findOne({ token });
+//     if (!user) {
+//       return res.status(401).json({ error: 'invalid token' });
+//     }
+
+//     const { id } = req.query;
+    
+//     // Find todo by _id in embedded array
+//     const todo = user.todos.id(id);
+//     if (!todo) {
+//       return res.status(404).json({ error: 'not found' });
+//     }
+
+//     // Toggle done status
+//     todo.done = !todo.done;
+//     todo.updatedAt = new Date();
+    
+//     await user.save();
+    
+//     return res.json(todo);
+//   } catch (error) {
+//     console.error('Toggle todo error:', error);
+//     return res.status(500).json({ error: 'server error' });
+//   }
+// }
